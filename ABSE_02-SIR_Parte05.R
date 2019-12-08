f1cf6e975f5884a889e31f70ca28acec59d4c542
Métodos Exatos <37481514+metodosexatos@users.noreply.github.com>
2019-12-08 12:27:10 -0300

    Add files via upload

---
+++ ABSE_02-SIR_Parte05.R
@@ -0,0 +1,59 @@
+#********************************************************#
+# www.metodosexatos.com                                  #
+# Prof.Ms. André Santos | andre@metodosexatos.com.br     #
+# Curso Análise Bayesiana: Simulação Estocástico com R   #
+# Re-Amostragem por Importância                          #
+# Data: 08/12/2019                                       #
+#********************************************************#
+
+# SIR::Sample Importance Resampling (Re-Amostragem por Importância)
+
+#************************** Exemplo RU 486 ******************************#
+
+#::::::::::::::::::::::: Processos essenciais :::::::::::::::::::::::::::#
+
+# Amostra de teta:
+n <- 10^5
+set.seed(42)
+teta <- runif(n, 0, 1)
+
+# Verossimilhança para cada valor de teta:
+ver <- dbinom(0, 4, prob = teta)
+
+# Densidade da priori para os valores de teta
+priori <- dunif(teta, 0, 1)
+
+# Função de importância
+imp <- dunif(teta, 0, 1)
+
+# Pesos normalizados
+pesos <- (ver*priori)/imp
+pesos.norm <- pesos/sum(pesos)
+
+# Amostra com reposição
+set.seed(123)
+teta.sir <- sample(teta, size = 1000, replace = TRUE, prob = pesos.norm)
+
+# Entropia Relativa à Uniformidade
+eru <- -sum(pesos.norm*log(pesos.norm))/log(n)
+print(eru)
+
+# Resultados
+quantile(teta.sir, c(0.025, 0.25, 0.5, 0.75, 0.975))
+mean(teta.sir)
+sd(teta.sir)
+
+# Modelo teórico (analítico)
+set.seed(123)
+beta <- rbeta(n, 1, 5)
+qbeta(c(0.025, 0.25, 0.5, 0.75, 0.975), 1, 5)
+mean(beta)
+sd(beta)
+
+# Histogramas
+hist(teta, main = "Distribuição Priori")
+hist(ver, main = "Verossimilhança")
+hist(teta.sir, main = "Distribuição Posterior")
+hist(beta, main = "Modelo Teórico (Analítico)")
+
+     
